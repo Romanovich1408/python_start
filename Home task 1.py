7ce6618e3e99d215909b6307ccd78d2fe042534a
Romanovich1408 <94222804+Romanovich1408@users.noreply.github.com>
2021-11-15 00:37:11 +0200

    Add files via upload

---
+++ Home task 1.py
@@ -0,0 +1,19 @@
+# task 1
+print('hello word')
+# task 2
+print('Максим', 'учится', 'piton')
+# task 3
+x = int(input('длина'))
+y = int(input('ширина'))
+print(x * y)
+# task 4
+x = int(input('x='))
+y = int(input('y='))
+print(x + y, x - y, x / y, x * y)
+
+
+
+
+
+
+
